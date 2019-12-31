9e113692140b45b1ca42c9b6c47ba9baa6dfb3d3
Mike Hendriks <mikehen6@gmail.com>
2019-12-31 01:15:37 +0100

    View product external button added. styling improvements

--- src/components/post/slices/GoogleDoc.js
+++ src/components/post/slices/GoogleDoc.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { RichText } from 'prismic-reactjs';
 
 const GoogleDoc = ({ slice }) => {
+  const docSrc = RichText.asText(slice.primary.document);
   return (
     <div className="post-part single docEmbed">
       <div className="container">
-        <iframe src={RichText.asText(slice.primary.document)} width="100%" height="900px"></iframe>
+        <a href={docSrc} target="_blank">Bekijk document</a>
+        <iframe src={docSrc} width="100%" height="900px"></iframe>
       </div>
     </div>
   );
