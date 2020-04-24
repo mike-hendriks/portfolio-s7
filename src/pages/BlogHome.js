394d0cb690106b4eed497d8bc9b01f4cb9759801
Mike Hendriks <mikehen6@gmail.com>
2020-04-24 13:33:40 +0200

    Configuration updated for s6.2 cms

--- src/pages/BlogHome.js
+++ src/pages/BlogHome.js
@@ -1,41 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import { RichText } from 'prismic-reactjs';
-import { Predicates } from 'prismic-javascript';
+import React, { useEffect, useState } from "react";
+import { RichText } from "prismic-reactjs";
+import { Predicates } from "prismic-javascript";
 
-import { Header, PostList, DefaultLayout, Menu } from '../components';
-import NotFound from './NotFound';
-import { TextBlock } from './../components/blog-home';
-import { client } from '../utils/prismicHelpers';
+import { Header, PostList, DefaultLayout, Menu } from "../components";
+import NotFound from "./NotFound";
+import { TextBlock } from "./../components/blog-home";
+import { client } from "../utils/prismicHelpers";
 
 /**
  * Blog homepage component
  */
 const BlogHome = () => {
-  const [prismicData, setPrismicData] = useState({ homeDoc: null, blogPosts: null, menuDoc: null});
+  const [prismicData, setPrismicData] = useState({
+    homeDoc: null,
+    blogPosts: null,
+    menuDoc: null,
+  });
   const [notFound, toggleNotFound] = useState(false);
 
   // Get the homepage and blog post documents from Prismic
   useEffect(() => {
     const fetchPrismicData = async () => {
       try {
-        const homeDoc = await client.getSingle('blog_home');
-        const menuDoc = await client.getSingle('menu');
+        const homeDoc = await client.getSingle("blog_home");
+        const menuDoc = await client.getSingle("menu");
         const blogPosts = await client.query(
-          Predicates.at('document.type', 'post'),
-          { orderings: '[my.post.date desc]' }
+          Predicates.at("document.type", "post"),
+          { orderings: "[my.post.date desc]" }
         );
-  
+
         if (homeDoc) {
           setPrismicData({ homeDoc, blogPosts: blogPosts.results, menuDoc });
         } else {
-          console.warn('Blog Home document was not found. Make sure it exists in your Prismic repository');
+          console.warn(
+            "Blog Home document was not found. Make sure it exists in your Prismic repository"
+          );
           toggleNotFound(true);
         }
       } catch (error) {
         console.error(error);
         toggleNotFound(true);
       }
-    }
+    };
 
     fetchPrismicData();
   }, []);
@@ -47,31 +53,36 @@ const BlogHome = () => {
     const blogPosts = prismicData.blogPosts;
     const title = RichText.asText(homeDoc.data.headline);
     return (
-
       <DefaultLayout seoTitle={title}>
         <Menu menuDoc={menuDoc}></Menu>
         <Header
           headline={homeDoc.data.headline}
           description={homeDoc.data.description}
         />
-        {homeDoc.data.assignment_block_title[0].text &&
-          <TextBlock title={homeDoc.data.assignment_block_title} content={homeDoc.data.assignment_block_content} />
-        }
+        {homeDoc.data.assignment_block_title[0].text && (
+          <TextBlock
+            title={homeDoc.data.assignment_block_title}
+            content={homeDoc.data.assignment_block_content}
+          />
+        )}
         <div className="container textSection">
           <h2>Alle opdrachten</h2>
           <div className="blog-main">
             <PostList posts={blogPosts} />
           </div>
         </div>
-        {homeDoc.data.reflection_block_title[0].text &&
-          <TextBlock title={homeDoc.data.reflection_block_title} content={homeDoc.data.reflection_block_content} />
-        }
+        {homeDoc.data.reflection_block_title[0].text && (
+          <TextBlock
+            title={homeDoc.data.reflection_block_title}
+            content={homeDoc.data.reflection_block_content}
+          />
+        )}
       </DefaultLayout>
     );
   } else if (notFound) {
     return <NotFound />;
   }
   return null;
-}
+};
 
 export default BlogHome;
